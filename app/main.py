f5d220a64d5fffa17e2876eb81c9eb440e2a4eab
gmadalosso <gbmadalosso@gmail.com>
2026-01-02 14:38:05 -0300

    adicionado template HTML

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+
 from app.routes.calendario import router as calendario_router
 
 app = FastAPI(title="Gerador de Calendário")
+
+templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(calendario_router)
--- app/routes/calendario.py
+++ app/routes/calendario.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/", response_class=HTMLResponse)
-async def root():
-    return "<h1>gerador de calendário funcionando</h1>"
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "calendario.html",
+        {"request": request}
+    )
