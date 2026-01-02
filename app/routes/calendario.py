2eaf8a0f3ab964d0222ef9747deade3bd1ac4a2a
gmadalosso <gbmadalosso@gmail.com>
2026-01-02 17:22:04 -0300

    atualizada rota do calendário

--- app/routes/calendario.py
+++ app/routes/calendario.py
@@ -1,22 +1,44 @@
 from datetime import datetime
+
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from app.core.enums import (
+    InicioSemana,
+    MAPA_INICIO_SEMANA,
+    COLUNAS_DIAS_SEMANA
+)
+
+from app.services.calendario_service import gerar_calendario_anual
+
+
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
+
 @router.get("/", response_class=HTMLResponse)
-async def calendario (
+async def calendario(
     request: Request,
-    year: int | None = None
+    ano: int | None = None,
+    inicio_semana: InicioSemana = InicioSemana.DOMINGO
 ):
-    year = year or datetime.now().year
+    ano = ano or datetime.now().year
+
+    primeiro_dia_semana = MAPA_INICIO_SEMANA[inicio_semana.value]
+
+    datas_calendario = gerar_calendario_anual(
+        ano=ano,
+        primeiro_dia_semana=primeiro_dia_semana
+    )
 
     return templates.TemplateResponse(
         "calendario.html",
         {
             "request": request,
-            "year": year
+            "ano": ano,
+            "calendario": datas_calendario,
+            "dias_semana": COLUNAS_DIAS_SEMANA[inicio_semana.value],
+            "inicio_semana": inicio_semana.value
         }
     )
